524b595c2629dcf7f090c12834b3b3886244ed99
edmundo papi <edmundopierre@yahoo.com>
2020-07-09 16:18:48 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,22 @@
+class Song
+attr_accessor :artist, :name
+@@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+def self.new_by_filename(file)
+  parse = file.split(" - ")[1]
+  song = Song.new(parse)
+  song.artist
+end
+
+def artist_name=(name)
+artist = Artist.new
+Song.artist
+end
+end
